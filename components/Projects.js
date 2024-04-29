703820a10701c4071ae51956659356d4cc54b71a
unknown <tyler.r.roll@gmail.com>
2024-04-29 16:15:08 -0600

    adding blkswan

--- components/Projects.js
+++ components/Projects.js
@@ -9,13 +9,16 @@ import social from '../Images/4.png'
 import dao from '../Images/3.png'
 import token from '../Images/2.png'
 import appchain from '../Images/1.png'
+import blkswan from '../Images/blkswan.png'
 
 function Projects() {
   return (
     <div className={styles.projects}>
       {/* <Image src={ashland} height={400} width={800}></Image> */}
       <h4>Plan 3 Site</h4>
-      <Link href="https://plan3alphasite.vercel.app/"><Image src={plan3} height={400} width={800}></Image></Link>
+      <Link href="https://plan3alphasite.vercel.app/"><Image src={plan3} height={400} width={1200}></Image></Link>
+      <h4>Black Swan Research</h4>
+      <Link href="https://plan3alphasite.vercel.app/"><Image src={blkswan} height={400} width={1200}></Image></Link>
       <h4>Social media web scraper</h4>
       <Link href="https://github.com/mediocreRlplayer/socialScraper"><Image src={social}></Image></Link>
       <h4>DeFi App Chain</h4>
